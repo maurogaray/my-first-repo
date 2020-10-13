dec1eab4ab057eec5b1507ccd87ed709326564d7
maurogaray <maurogaray32@gmail.com>
2020-10-13 12:00:02 +0200

    Deffinition added to the exercise document IS EVEN

--- exercise.py
+++ exercise.py
@@ -6,3 +6,17 @@ Created on Tue Oct 13 11:46:37 2020
 @author: maurogaray
 """
 
+def is_even(): 
+    
+    num = int(input("Please enter a number: "))    
+    if ( num % 2 ) == 0:
+        print( "This number  is even")
+    else:
+        print( " This number is odd ")
+        
+is_even()
+
+
+
+
+
